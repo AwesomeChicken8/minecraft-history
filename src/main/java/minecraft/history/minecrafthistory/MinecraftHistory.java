4e924053fa934fefee4ff9c8d74cc3a818a034b4
Sammy Reifel <sammyreifel@Sammys-Air.fios-router.home>
2022-12-12 18:42:05 -0500

    Initial commit

---
+++ src/main/java/minecraft/history/minecrafthistory/MinecraftHistory.java
@@ -0,0 +1,17 @@
+package minecraft.history.minecrafthistory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftHistory extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
